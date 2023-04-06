3d2f8d194d5525d1fb5dfc03fec94c1625fc17b3
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-06 00:19:49 +0000

    add npm packages, cheerio, axios

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
 const express = require('express')
 const app = express()
+
+const url = 'https://www.yabiladi.ma/prieres/'
+
+app.get('/result', (req, res) => {
+    axios(url).
+        then(response => {
+            const html = response.data
+            console.log(html)
+            const $ = cheerio.load(html)
+            const content = []
+
+            
+        })
+})
 
 app.listen(3000, () => console.log("Yeah I'm Running"))
