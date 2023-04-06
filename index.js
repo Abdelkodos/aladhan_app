8983b05cc3987b637fc7520192e79a13b07fdb21
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-04-06 23:45:57 +0000

    disable .index.js.swp

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ app.get('/result', (req, res) => {
     axios(url).
         then(response => {
             const html = response.data
-            console.log(html)
             const $ = cheerio.load(html)
             const content = []
 
-            
-        })
+            $('.corps', html).each(function () {
+                
+                const prays = $(this).find('table').children().text()
+                content.push({
+                    prays
+                })
+            })
+
+            res.json(content)
+        }).catch((err) => console.log(err))
 })
 
 app.listen(3000, () => console.log("Yeah I'm Running"))
