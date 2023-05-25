9fefa3f99ba2c9ce926fb14a3fdc76bcd402bcf0
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-05-26 00:09:59 +0100

    restructuring the data display

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const readline = require('readline')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
@@ -5,6 +7,48 @@ const app = express()
 
 /* Scraped site URL */
 const url = 'https://www.yabiladi.ma/prieres/'
+
+const saveData = async (content) => {
+	await fs.writeFile('data.json', new Date() + "\n" + content, function (err) {
+		if (err) throw err
+		console.log('Saved!')
+	})
+}
+
+const structData = (data) => {
+	let cities = {}
+	if (data.includes(":الرباط"))
+	{
+		cities.rabat = []
+		let cityIndex = data.indexOf(":الرباط")
+		console.log(cityIndex)
+		console.log(data.indexOf("04:36"))
+		console.log(data.indexOf("21:59"))
+/*		for (let i = (cityIndex + 7);
+		cities += '"rabat": ['
+		for ( let i = (cityIndex + 7); i < (cityIndex + 40); i++)
+		{
+			for (let j = i; j < (i + 5); j++)
+			{
+				cities += `"${data.charAt(i) + data.charAt(i+1) + data.charAt(i+2) + data.charAt(i+3) + data.charAt(i+4) + data.charAt(i+5)}"`
+			}
+		}
+		cities += "]}"
+		console.log(cities)*/
+	}
+}
+
+const splitString = (content) => {
+	let arrcont = content.split("\t").join("-")
+	arrcont = arrcont.split(" ").join("*")
+	arrcont = arrcont.split("************************").join(":")
+	arrcont = arrcont.split("**-***********\n").join("")
+	arrcont = arrcont.split("---****").join("")
+	arrcont = arrcont.split("---****\n").join("")
+	arrcont = arrcont.split("---***\n").join("")
+	arrcont = arrcont.split("-:").join(":")
+	return arrcont
+}
 
 app.get('/result', (req, res) => {
     axios(url).
@@ -14,14 +58,17 @@ app.get('/result', (req, res) => {
             const content = []
 
             $('.corps', html).each(function () {
-                
+
                 const prays = $(this).find('table').children().text()
                 content.push({
                     prays
                 })
             })
-
-            res.json(content)
+	    let praysSaved = "prays: " + splitString(content[0].prays)
+	    console.log(praysSaved)
+	    structData(praysSaved)
+            res.send(praysSaved)
+	    saveData(praysSaved)
         }).catch((err) => console.log(err))
 })
 
