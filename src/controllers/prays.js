446097cf5f108f737db02d094437bdcebff88335
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2023-11-27 21:34:45 +0100

    optimize the code by adding a new algorithm

--- src/controllers/prays.js
+++ src/controllers/prays.js
@@ -277,98 +277,15 @@ const getAdhanByCityId = (req, res) => {
 						})
 					}).catch((err) => console.log(err))
 	}
-	
-		if ( id == 1 ) {
-			scrapping("casablanca")
-		}
-		if ( id == 2 ) {
-			scrapping("rabat")
-		}
-		if ( id == 3 ) {
-			scrapping("fes")
-		}
-				
-		if ( id == 4) {
-				scrapping("tanger")
-			}
-		if ( id == 5 ) {
-			scrapping("marrakech")
-		}
-			
-		if ( id == 6 ) {
-			scrapping("agadir")
-		}
-			
-		if ( id == 7) {
-			scrapping("meknes")
-		}
-			
-		if ( id == 8 ) {
-			scrapping("oujda")
-		}
-			
-		if ( id == 9) {
-			scrapping("laayoune")
-		}
-			
-		if ( id == 10 ) {
-			scrapping("dakhla")
-		}
-			
-		if ( id == 11 ) {
-			scrapping("essaouira")
-		}
-			
-		if ( id == 12 ) {
-			scrapping("temara")
-		}
-			
-		if ( id == 13 ) {
-			scrapping("tetouan")
-		}
-			
-		if ( id == 14 ) {
-			scrapping("alhoceima")
-		}
-			
-		if ( id == 15 )
-			scrapping("benimellal")
 
-		if ( id == 16 )
-			scrapping("nador")
-			
-		if ( id == 17 )
-			scrapping("safi")
-			
-		if ( id == 18 )
-			scrapping("khouribga")
-				
-		if ( id == 19 )
-			scrapping("eljadida")
-				
-		if ( id == 20 )
-			scrapping("berkane")
+		const cityChoosed = cities.find(city => city.id == id)
 
-		if ( id == 21 )
-			scrapping("taza")
-			
-		if ( id == 22 )
-			scrapping("mohammedia")
-			
-		if ( id == 23 )
-			scrapping("khemisset")
-			
-		if ( id == 24 )
-			scrapping("errachidia")
-			
-		if ( id == 25 )
-			scrapping("kenitra")
-			
-		if ( id == 26 )
-			scrapping("larache")
+		if ( cityChoosed )
+			scrapping(cityChoosed.name)
 
-		if ( id > 26 || id < 1)
+		else
 			res.status(404).send({message : "there is no city with this number"})
+
 }
 
 
