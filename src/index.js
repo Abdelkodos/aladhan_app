d40a239d541a89eda564ff3f91db3e37101a9aa7
Abdelkodos <abdelkoudous.mekkaoui@gmail.com>
2024-03-15 18:19:52 +0000

    add new route of user fetching

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import { fileURLToPath } from 'url'
 import { readFileSync } from 'fs'
 import path from 'path'
 
-const data2 = JSON.parse(readFileSync(new URL('./second_data.json', import.meta.url)))
+const data = JSON.parse(readFileSync(new URL('./second_data.json', import.meta.url)))
+const users = JSON.parse(readFileSync(new URL('./users.json', import.meta.url)))
 const app = express()
+
+const getUser = id => users.map((user) => user.userId == id ? user : null )
+const getData = id => data.map((data) => data.userId == id ? data : null )
 
 app.use(cors({
 	origin: '*',
@@ -21,8 +25,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/prays", getAllAdhans)
 app.get('/prays/:id', getAdhanByCityId)
-app.get('/data', (req, res) => {
-	res.json(data2)
+app.get('/data/:id', (req, res) => {
+	const id = req.params.id
+	res.json(getData(id))
+})
+app.get('/user/:id', (req, res) => {
+	const id = req.params.id
+	res.json(getUser(id))
 })
 
 app.listen(3000, () => console.log("Yeah I'm Running"))
